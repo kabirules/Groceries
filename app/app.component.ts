90563f030321a2c909d1ea02a82eaee81f85e97a
Javier Font <javifont@gmail.com>
2017-08-15 10:52:12 +0200

    Chapter 3.4 finished

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { User } from "./shared/user/user";
 import { Component } from "@angular/core";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
+  providers: [UserService],
   templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
@@ -11,11 +13,30 @@ export class AppComponent {
   user: User;
   isLoggingIn = true;
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.user = new User();
+    this.user.email = "jf@gmail.com1";
+    this.user.password = "1234";
   }
   submit() {
-    alert("You’re using: " + this.user.email);
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // TODO: Define
+  }
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
   }
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
