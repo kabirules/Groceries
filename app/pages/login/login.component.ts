9a4c7e70a5f242da2ad96fbcae51ea2c63d6c325
Javier Font <javifont@gmail.com>
2017-09-08 19:56:38 +0200

    Chapter 3 finished

---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,49 @@
+import { User } from "../../shared/user/user";
+import { Component } from "@angular/core";
+import { UserService } from "../../shared/user/user.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+
+export class LoginComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private router: Router, private userService: UserService) {
+    this.user = new User();
+    this.user.email = "jf@gmail.com1";
+    this.user.password = "1234";
+  }
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    this.userService.login(this.user)
+      .subscribe(
+        () => this.router.navigate(["/list"]),
+        (error) => alert("Unfortunately we could not find your account.")
+      );
+  }
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+};
